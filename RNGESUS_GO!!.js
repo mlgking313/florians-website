124be077886a9b364df2b624226e4de6ae100d38
mlgking313 <161741526+mlgking313@users.noreply.github.com>
2024-10-23 12:46:11 +0200

    Update RNGESUS_GO!!.js

--- RNGESUS_GO!!.js
+++ RNGESUS_GO!!.js
@@ -1,14 +1,8 @@
-const  = document.getElementById('rand');
-
-rand.addEventListener('click', () => {
-  const linkie = document.getElementById('linkie');
-  var hi = Math.floor(Math.random() * 3);
-  if (hi == 2) {
-  linkie.href = 'stats.html';
-  } else if (hi == 1) {
-    linkie.href = 'index.html';
-  } else {
-    linkie.href = 'about.html';
-    
-  return false;
-});
+const ok = Math.floor(Math.random() *3);
+if (ok==1) {
+document.getElementById("p1").innerHTML = "New text!";
+} else if (ok==2) {
+document.getElementById("p1").innerHTML = "cool";
+} else {
+document.getElementById("p1").innerHTML = "ayo";
+}
