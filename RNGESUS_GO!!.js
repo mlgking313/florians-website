ddf757f9a5f2d4e453dee4a72bdb6b67359f5c47
mlgking313 <161741526+mlgking313@users.noreply.github.com>
2024-10-27 15:21:11 +0100

    Delete RNGESUS_GO!!.js

--- RNGESUS_GO!!.js
+++
@@ -1,8 +0,0 @@
-const ok = Math.floor(Math.random() *3);
-if (ok==1) {
-document.getElementById("p1").innerHTML = "New text!";
-} else if (ok==2) {
-document.getElementById("p1").innerHTML = "cool";
-} else {
-document.getElementById("p1").innerHTML = "ayo";
-}
