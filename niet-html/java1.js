51fac9ce79a626d9bf6a9f0c40d137b922584c9a
mlgking313 <161741526+mlgking313@users.noreply.github.com>
2025-11-17 09:26:42 +0100

    Update java1.js

--- niet-html/java1.js
+++ niet-html/java1.js
@@ -2,8 +2,10 @@
 const ok = Math.floor(Math.random() *100);
 if (ok < 50) {
 document.getElementById("p1").href = "about.html";
-} else if (ok > 50) {
+} else if (ok > 50&& ok < 75) {
 document.getElementById("p1").href="stats.html";
-} else {
+} else if (ok >75) {
+  document.getElementById("p1").href="beeg_games.html";
+}  else {
 document.getElementById("p1").href="secretpage.html";
 }
